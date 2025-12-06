d7685a472a741aff5135268c396e180094b68b3d
Keiran <keircn@proton.me>
2025-12-06 07:58:35 +0000

    add projects

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -8,24 +8,33 @@ export type Project = {
 
 export const projects: Project[] = [
   {
-    name: "empty title",
-    description: "empty description",
-    tags: ["tags", "go", "here"],
-    href: "#",
-    images: [],
+    name: "Priory",
+    description:
+      "Share files and create short links with ease. Simple, secure, and private. ",
+    tags: ["NextJS", "Drizzle", "TailwindCSS", "R2"],
+    href: "https://github.com/bestire/priory",
+    images: [
+      "https://cdn.nest.rip/uploads/2841d39c-0f4e-4e26-acce-49ebf2d90424.png",
+    ],
   },
   {
-    name: "empty title",
-    description: "empty description",
-    tags: ["tags", "go", "here"],
-    href: "#",
-    images: [],
+    name: "hostman",
+    description:
+      "Fast, customizable file upload tool with support for any image host you can throw at it.",
+    tags: ["C", "Cmake", "SQLite", "cURL", "JSON"],
+    href: "https://github.com/bestire/hostman",
+    images: [
+      "https://cdn.nest.rip/uploads/3cddbee1-bfc8-47c7-97c8-e6e8ae36717e.png",
+      "https://cdn.nest.rip/uploads/dcfb82e9-f803-4290-b4cc-7f1d71a826b0.png",
+    ],
   },
   {
-    name: "empty title",
-    description: "empty description",
-    tags: ["tags", "go", "here"],
-    href: "#",
-    images: [],
+    name: "kcst",
+    description: "Temporary file hosting",
+    tags: ["Go", "TOML", "Cloudflare"],
+    href: "https://cdn.keircn.com",
+    images: [
+      "https://cdn.nest.rip/uploads/3d243441-e34d-4b59-94f9-54863bcbefb6.png",
+    ],
   },
 ];
